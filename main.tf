24bba790850ca2ba00ef0b9c6d66fa5b56c7e6a8
CaglaNurArslan <117541189+CaglaNurArslan@users.noreply.github.com>
2023-02-04 16:32:37 -0500

    dijf_

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_iam_user" "this" {
   name = "user_created_by_terraform_cloud"
 }
 resource "aws_iam_user" "this2" {
-  name = "user_created_by_terraform_cloud"
+  name = "user_created_by_terraform_cloud2"
 }
